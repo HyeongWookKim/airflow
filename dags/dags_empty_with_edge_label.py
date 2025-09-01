126bb57ee6413f9d0d23043e697a715630ff0da8
hwkim <herry1021@gmail.com>
2025-09-01 11:42:21 +0900

    Edge Label 샘플 dag 업로드

---
+++ dags/dags_empty_with_edge_label.py
@@ -0,0 +1,47 @@
+import pendulum
+
+# Airflow 3.0 버전부터 아래 경로에서 import
+from airflow.sdk import DAG, Label
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+# Airflow 2.10.5 이하 버전에서 실습 시, 아래 경로에서 import
+# from airflow import DAG
+# from airflow.utils.edgemodifier import Label
+# from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id = 'dags_empty_with_edge_label',
+    schedule = None,
+    start_date = pendulum.datetime(2023, 4, 1, tz = 'Asia/Seoul'),
+    catchup = False
+) as dag:
+
+    # 예시 1
+    empty_1 = EmptyOperator(
+        task_id = 'empty_1'
+    )
+
+    empty_2 = EmptyOperator(
+        task_id = 'empty_2'
+    )
+    
+    empty_1 >> label('1과 2 사이') >> empty_2
+
+    # 예시 2
+    empty_3 = EmptyOperator(
+        task_id = 'empty_3'
+    )
+
+    empty_4 = EmptyOperator(
+        task_id = 'empty_4'
+    )
+
+    empty_5 = EmptyOperator(
+        task_id = 'empty_5'
+    )
+
+    empty_6 = EmptyOperator(
+        task_id = 'empty_6'
+    )
+
+    empty_2 >> Label('Start Branch') >> [empty_3, empty_4, empty_5] >> Label('End Branch') >> empty_6
