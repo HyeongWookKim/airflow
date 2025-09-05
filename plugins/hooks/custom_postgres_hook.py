9775061a6ba1344a372e7eb1325ccf3209d3273f
hwkim <herry1021@gmail.com>
2025-09-05 17:13:17 +0900

    Custom Hook with bulk_load dag 코드 수정

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -44,12 +44,14 @@ class CustomPostgresHook(BaseHook):
                 
         self.log.info(f'적재 건수: {str(len(df_file))}')
 
-        uri = f'postgresql://{self.user}:{self.password}@{self.host}/{self.dbname}'
+        uri = f'postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}'
         engine = create_engine(uri)
-        df_file.to_sql(
-            name = table_name,
-            con = engine,
-            schema = 'public',
-            if_exists = if_exists,
-            index = False
-        )
+        
+        with engine.connect() as conn:
+            df_file.to_sql(
+                name = table_name,
+                con = conn,
+                schema = 'public',
+                if_exists = if_exists,
+                index = False
+            )
