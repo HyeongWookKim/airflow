53a454b71a1df91e0f23c42f930b8129f0a2e5b5
hwkim <herry1021@gmail.com>
2025-09-10 16:12:46 +0900

    DateTimeSensor with async 샘플 dag start & end date 수정

--- dags/dags_time_sensor.py
+++ dags/dags_time_sensor.py
@@ -10,8 +10,8 @@ from airflow.providers.standard.sensors.date_time import DateTimeSensor
 
 with DAG(
     dag_id = 'dags_time_sensor',
-    start_date = pendulum.datetime(2023, 5, 1, 0, 0, 0),
-    end_date = pendulum.datetime(2023, 5, 1, 1, 0, 0),
+    start_date = pendulum.datetime(2025, 9, 1, 0, 0, 0),
+    end_date = pendulum.datetime(2025, 9, 1, 1, 0, 0),
     schedule = '*/10 * * * *', # 10분마다 한 번씩 스케줄 실행 -> 따라서 총 7번(당일 00시 ~ 다음 날 00시) 실행됨
     catchup = True,
 ) as dag:
