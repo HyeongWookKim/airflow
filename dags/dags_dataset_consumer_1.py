301bb1ee93538d732e4a20dd1d7fc87a892cc2ea
hwkim <herry1021@gmail.com>
2025-09-10 10:34:46 +0900

    Dataset - Producer & Consumer 샘플 dag 업로드

---
+++ dags/dags_dataset_consumer_1.py
@@ -0,0 +1,25 @@
+import pendulum
+
+# Airflow 3.0 버전부터 아래 경로에서 import
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG, Asset
+
+# Airflow 2.10.5 이하 버전에서 실습 시, 아래 경로에서 import
+# from airflow.operators.bash import BashOperator
+# from airflow import DAG
+# from airflow import Dataset
+
+asset_dags_asset_producer_1 = Asset('dags_dataset_producer_1') # Publish 했던 key 값 입력
+
+with DAG(
+        dag_id = 'dags_dataset_consumer_1',
+        schedule = [asset_dags_asset_producer_1], # 구독할 key 값을 리스트에 입력
+        start_date = pendulum.datetime(2023, 4, 1, tz = 'Asia/Seoul'),
+        catchup = False,
+        tags = ['asset','consumer']
+) as dag:
+    
+    bash_task = BashOperator(
+        task_id = 'bash_task',
+        bash_command = 'echo {{ ti.run_id }} && echo "producer_1 이 완료되면 수행"'
+    )
